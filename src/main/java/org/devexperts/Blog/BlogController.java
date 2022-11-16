8d137f8cde5345aa4c1ab14be0cbf9bfd2189c2e
Tahir Iqbal <101194288+mtahiriqbaldeveloper@users.noreply.github.com>
2022-11-16 17:02:42 +0100

    spring core web-framework dependency

--- src/main/java/org/devexperts/Blog/BlogController.java
+++ src/main/java/org/devexperts/Blog/BlogController.java
@@ -1,5 +1,18 @@
 package org.devexperts.Blog;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController("/api")
 @Controller
 public class BlogController {
+
+    @GetMapping("/status")
+    public ResponseEntity<?> getResponse(){
+
+        return ResponseEntity.ok("hi its controller response");
+    }
+
 }
---
+++ src/main/java/org/devexperts/Blog/main.java
@@ -0,0 +1,8 @@
+package org.devexperts.Blog;
+
+public class main {
+
+    public static void main(String[] args){
+        BlogController blogController = new BlogController();
+    }
+}
